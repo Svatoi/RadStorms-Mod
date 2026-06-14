93e6b277ea50f42494a4f0675433a8da4ac941d7
Svatoi <privitvlad00@gmail.com>
2026-06-14 16:03:32 +0200

    update fog despawn

--- src/main/java/com/radsto/radstorms/event/ModClientEvents.java
+++ src/main/java/com/radsto/radstorms/event/ModClientEvents.java
@@ -31,9 +31,25 @@ public class ModClientEvents {
             int playerY = pos.getY();
             int surfaceY = level.getHeight(Heightmap.Types.WORLD_SURFACE, pos.getX(), pos.getZ());
 
-            if (playerY > surfaceY - 10) {
+            int fadeStart = surfaceY;
+            int fadeEnd = surfaceY - 15;
+
+            if (playerY >= fadeStart) {
                 event.setNearPlaneDistance(1.3f);
                 event.setFarPlaneDistance(22f);
+                event.setCanceled(true);
+            } else if (playerY > fadeEnd) {
+                float totalDistance = fadeStart - fadeEnd;
+                float playerDistance = fadeStart - playerY;
+                float depthFactor = playerDistance / totalDistance;
+
+                float startFar = 22f;
+                float endFar = 25f;
+                float currentFar = startFar + (endFar - startFar) * depthFactor;
+
+                float currentNear = 1.5f + (8f - 1.3f) * depthFactor;
+                event.setNearPlaneDistance(currentNear);
+                event.setFarPlaneDistance(currentFar);
                 event.setCanceled(true);
             }
         }
@@ -51,14 +67,29 @@ public class ModClientEvents {
 
         boolean isRaining = level.isRaining() || level.isThundering();
 
-        if (isRaining) {
+        if (isRaining && isClientStormActive) {
             int playerY = pos.getY();
             int surfaceY = level.getHeight(Heightmap.Types.WORLD_SURFACE, pos.getX(), pos.getZ());
+
+            int fadeStart = surfaceY;
+            int fadeEnd = surfaceY - 15;
 
             if (playerY > surfaceY - 10) {
                 event.setRed(0.45f);
                 event.setGreen(0.45f);
                 event.setBlue(0.45f);
+            } else if (playerY > fadeEnd) {
+                float totalDistance = fadeStart - fadeEnd;
+                float playerDistance = fadeStart - playerY;
+                float depthFactor = playerDistance / totalDistance;
+
+                float vanillaR = event.getRed();
+                float vanillaG = event.getGreen();
+                float vanillaB = event.getBlue();
+
+                event.setRed(0.45f + (vanillaR - 0.45f) * depthFactor);
+                event.setGreen(0.45f + (vanillaG - 0.45f) * depthFactor);
+                event.setBlue(0.45f + (vanillaB - 0.45f) * depthFactor);
             }
         }
     }
